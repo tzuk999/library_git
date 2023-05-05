3f9745391336f680d887e980149effced89a174a
ranerlich-johnbrice <ranerlich@gmail.com>
2023-05-05 12:09:08 +0300

    requirments.txt added + some changes

--- book.py
+++ book.py
@@ -1,5 +1,13 @@
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
+
 print("books")
 print("books2222")
 print("new print")
 
 print(" Anna's commit")
+
